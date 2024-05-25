0bfd084aa81a016501be7807582323f3f46658ec
SahilNijhawan02 <139138597+SahilNijhawan02@users.noreply.github.com>
2024-05-25 18:32:09 +0530

    Add files via upload

---
+++ crudmodule/dao/ProductDao.java
@@ -0,0 +1,41 @@
+package crudmodule.dao;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.orm.hibernate5.HibernateTemplate;
+import org.springframework.stereotype.Component;
+
+import crudmodule.model.Product;
+import javax.transaction.Transactional;
+
+@Component
+public class ProductDao {
+	@Autowired
+	private HibernateTemplate hibernateTemplate;
+	
+	@Transactional
+	public void createProduct(Product product)
+	{
+		
+		this.hibernateTemplate.saveOrUpdate(product);
+	}
+	
+	public List<Product> getProducts()
+	{
+		 List<Product> products = this.hibernateTemplate.loadAll(Product.class);
+		 return products;
+	}
+	@Transactional
+	public void deleteProduct(int pid) {
+		
+		Product p = this.hibernateTemplate.load(Product.class, pid);
+		this.hibernateTemplate.delete(p);
+	}
+	
+	public Product getProduct(int pid)
+	{
+		return this.hibernateTemplate.get(Product.class, pid);
+	}
+
+}
